bc7d3fc4d465485e3e4c75a5ffd561c97664f181
macmini <joseph.yan@duke.edu>
2026-07-22 14:12:30 -0400

    created the public migration of the sql database with discogs and musicbrainz ids for as much releases.

--- migrations/002_release_claims.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS release_claims (
-    release_id  UUID        PRIMARY KEY REFERENCES releases(id),
-    reviewer    TEXT        NOT NULL,
-    claimed_at  TIMESTAMP   NOT NULL DEFAULT NOW()
-);
-
-CREATE INDEX IF NOT EXISTS release_claims_claimed_at_idx
-    ON release_claims (claimed_at);
